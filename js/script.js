e5a73e238d6c680aaf961b92b3392b68afbb2013
Stheven <sthevenalvess@gmail.com>
2024-02-08 14:50:58 -0300

    feat: handling exception

--- js/script.js
+++ js/script.js
@@ -4,13 +4,15 @@ const cityInput = document.querySelector('#city-input');
 const searchBtn = document.querySelector('#search');
 
 const cityElement = document.querySelector('#city');
-const tempElement = document.querySelector('#temperature span')
-const DescripElement = document.querySelector('#description')
-const WeatherIconElement = document.querySelector('#weather-icon')
+const tempElement = document.querySelector('#temperature span');
+const DescripElement = document.querySelector('#description');
+const WeatherIconElement = document.querySelector('#weather-icon');
 const CountyElement = document.querySelector('#country')
-const humidityElement = document.querySelector('#umidity span')
-const windElement = document.querySelector('#wind span')
+const humidityElement = document.querySelector('#humidity span');
+const windElement = document.querySelector('#wind span');
+const weatherContainer = document.querySelector('#weather-data');
 
+const errorMessageContainer = document.querySelector("#error-message");
 
 const getWeatherData = async (city) => {
     const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
@@ -23,15 +25,32 @@ const getWeatherData = async (city) => {
     return data;
 }
 
+const showErrorMessage = () => {
+    errorMessageContainer.classList.remove("hide");
+};
+
 const showWeatherData = async (city) => {
-    const data = await getWeatherData(city);
-    cityElement.innerText = data.name;
-    tempElement.innerText = parseInt(data.main.temp);
-    DescripElement.innerText = data.weather[0].description;
-    WeatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
-    CountyElement.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/64.png`);
-    humidityElement.innerText = `${data.main.humidity}%`;
-    windElement.innerText = `${data.wind.speed}km/h`;
+
+    try {
+        const data = await getWeatherData(city);
+        
+        if (data.cod === "404") {
+            showErrorMessage();
+            return;
+        }
+
+        cityElement.innerText = data.name;
+        tempElement.innerText = parseInt(data.main.temp);
+        DescripElement.innerText = data.weather[0].description;
+        WeatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+        CountyElement.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/64.png`);
+        humidityElement.innerText = `${data.main.humidity}%`;
+        windElement.innerText = `${data.wind.speed}km/h`;
+        weatherContainer.classList.remove("hide")
+    }
+    catch (err) {
+        console.log("erro: " + err)
+    }
 };
 
 
@@ -40,3 +59,10 @@ searchBtn.addEventListener("click", (e) => {
     const city = cityInput.value;
     showWeatherData(city);
 });
+
+cityInput.addEventListener("keyup", (e) => {
+    if (e.code === "Enter") {
+        const city = e.target.value;
+        showWeatherData(city);
+    }
+})
