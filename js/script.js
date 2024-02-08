4ba893d1fa53195844547b0d75f2aa6d7a680e2d
Stheven <sthevenalvess@gmail.com>
2024-02-08 13:26:35 -0300

    feat: create search event

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-//a4bc0456c041fd6ec8a1dd46729555cf
+const apiKey = "a4bc0456c041fd6ec8a1dd46729555cf";
+const apiCountryURL = "https://flagsapi.com/BR/flat/64.png";
+
+const cityInput = document.querySelector('#city-input');
+const searchBtn = document.querySelector('#search');
+
+const showWeatherData = (city) = {
+
+}
+
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const city = cityInput.value;
+    showWeatherData(city);
+})
