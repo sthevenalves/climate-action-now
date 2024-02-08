2e5c2a228d217dfc7e600ee0a1681a63c3791561
Stheven <sthevenalvess@gmail.com>
2024-02-08 13:46:31 -0300

    feat: consuming the openWeather API

--- js/script.js
+++ js/script.js
@@ -4,13 +4,32 @@ const apiCountryURL = "https://flagsapi.com/BR/flat/64.png";
 const cityInput = document.querySelector('#city-input');
 const searchBtn = document.querySelector('#search');
 
-const showWeatherData = (city) = {
+const cityElement = document.querySelector('#city');
+const tempElement = document.querySelector('#temperature span')
+const DescripElement = document.querySelector('#description')
+const WeatherIconElement = document.querySelector('#weather-icon')
+const CountyElement = document.querySelector('#country')
+const umidityElement = document.querySelector('#umidity span')
+const windElement = document.querySelector('#wind span')
+
+
+const getWeatherData = async (city) => {
+    const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherUrl);
+    const data = await res.json();
+
+    console.log(data);
 
 }
+
+const showWeatherData = (city) => {
+    getWeatherData(city);
+};
 
 
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
     const city = cityInput.value;
     showWeatherData(city);
-})
+});
